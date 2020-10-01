2ddf3e8e7b1307a17c39e37cb52b0beef13cd184
mdelgado <migueldelgadoangelo@gmail.com>
2020-10-01 08:47:27 -0500

    update music pairs algo

--- leetcode/musicPairs.js
+++ leetcode/musicPairs.js
@@ -4,14 +4,18 @@
  */
 
 let songs = [30, 20, 150, 100, 40];
-//find how many pairs may add to a multiple of 60
+//create an object to store durations
 let songsObject = songs.reduce((a,b) => (a[b] = 0, a), {})
 let pairs = 0;
 
 songs.forEach( song => {
+    //get duration
     let dur = song % 60
+    //set target (determine what value for it to be multiple of 60)
     let targ = (60 - dur) % 60
+    //add to pairs if found
     pairs += songsObject[targ] || 0
+    //add to object
     songsObject[dur] = (songsObject[dur] || 0) + 1
 });
 
